0d930f9022dbdd98b3827f2c5152847e4d4e954b
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2023-09-04 09:22:27 +0200

    lagt in kod för heltals uträkningar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subtraktion = tal1-tal2;
+
+        System.out.println("summan av tal1 + tal2 är "+summa_addition);
+        System.out.println("summan av tal1 - tal2 är "+summa_subtraktion);
+
+        int produkt = tal1*tal2;
+        int kvot = tal1/tal2;
+        System.out.println("summan av tal1 * tal 2 är "+produkt);
+        System.out.println("summan av tal1 / tal2 är "+kvot);
     }
 }
